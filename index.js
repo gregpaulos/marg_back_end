91cca142b71055627d25a645b75c781c260b2abe
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-12 16:20:31 -0600

    added cors body-parser and started to build connection to db

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require(`express`);
+const cors = require(`cors`);
+const bodyParser = require(`body-parser`);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get(`/v1/hello`, (req, res) => {
-    res.json({message: `Hello World!`})
-})
+  res.json({ message: `Hello World!` });
+});
+
+app.get(`/v1/establishments/:lat/:long`, (req, res) => {
+  queries
+    .getEstablishments(req.params.lat, req.params.long)
+    .then()
+    .catch();
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
