8f8e04969af7c567318471cc42b24d86bdc8aa46
Michael Schreier <mjschreier1@gmail.com>
2018-04-12 15:51:33 -0600

    completed simple hello world route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(`/hello`, (req, res) => {
+app.get(`/v1/hello`, (req, res) => {
     res.json({message: `Hello World!`})
 })
 
