1bcad5031abc4f02792df229471245475eb53e3b
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-16 13:28:13 -0600

    random route now returns distance with json

--- index.js
+++ index.js
@@ -27,9 +27,7 @@ async function calcDistance(long, lat, establishmentArray) {
     }
     return fetchArray  
   }
-
   const responseArray = await hitGoogle();
-  console.log('THIS IS OUR ARRAY OF GOOGLE INFO', responseArray);
   return responseArray
 }
 
@@ -53,28 +51,18 @@ app.get(`/v1/establishments/random/:long/:lat`, (req, res) => {
   console.log(req.params.lat);
   queries
     .getEstablishments()
-    .then(records => {
-      console.log('THIS IS FROM OUR DB, WE WANT TO PUT GOOGLE INFO ON IT', records);
-      
+    .then(records => { 
       let distanceArray = [];
       records.forEach(establishment => {
         distanceArray.push([establishment.long, establishment.lat]);
       });
       calcDistance(req.params.long, req.params.lat, distanceArray)
       .then(googleInfo => {
-        console.log('THIS IS WHAT WE GOT BACK', googleInfo);
-        googleInfo.forEach((distance, i) => {
-          console.log('INSIDE OF OUR FOR EACH', i, records[i], googleInfo[i]);
-          
+        googleInfo.forEach((distance, i) => {          
           records[i]["distance"] = googleInfo[i]
         })
+        res.json(records);
       })
-      return records;
-    })
-    .then(records => {
-      console.log('DOES THIS FIRE BEFORE');
-      
-      res.json(records);
     })
     .catch(err => {
       res.json({ error: err });
